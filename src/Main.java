7f6b044d79e6fbc2cb118189cddc1d3ffbdb8277
Miquel Debón Villagrasa <mdebonbcn@gmail.com>
2023-07-26 12:18:28 +0200

    done

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Es tracta de crear un generador de passwords. L'usuari ha d'indicar la mida del password i
+         * el programa ha de generar i mostrar per pantalla el password corresponent. El password pot contenir:
+         * minúscules, majúscules, números i aquests símbols:
+         * ('!','@','#','$','%','^','&','*','(',')','{','}','[',']','=','<','>')
+         *
+         * Hay que subirlo a GitHub y pasar el enlace al mentor por Discord.
+         * Buena suerte!
+         */
+
+        List<String> letras = List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+        List<Integer> numeros = List.of(1,2,3,4,5,6,7,8,9);
+        List<Character> characters = List.of('!','@','#','$','%','^','&','*','(',')','{','}','[',']','=','<','>');
+
+        int length = 10;
+        StringBuilder password = new StringBuilder();
+
+        for(int i=0; i<length; i++){
+            switch (randomNumber()){
+                case 1:
+                    password.append(letras.get(randomNumberInRange(letras)));
+                    break;
+                case 2:
+                    password.append(letras.get(randomNumberInRange(letras)).toUpperCase());
+                    break;
+                case 3:
+                    password.append(numeros.get(randomNumberInRange(numeros)));
+                    break;
+                case 4:
+                    password.append(characters.get(randomNumberInRange(characters)));
+                    break;
+            }
+        }
+        System.out.println(password);
+
+
+    }
+
+    static int randomNumber(){
+        return (int)(Math.random()*4)+1;
+    }
+
+    static int randomNumberInRange(List list){
+        int range = list.size()-1;
+        return (int)(Math.random()*range)+1;
+    }
+}
