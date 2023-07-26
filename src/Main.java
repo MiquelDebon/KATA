b68cc7b204bd3b8cd4b92191a6b6c2177c481901
Miquel Debón Villagrasa <mdebonbcn@gmail.com>
2023-07-26 12:25:53 +0200

    mejora de lectura

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,53 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    /**
+     * Es tracta de crear un generador de passwords. L'usuari ha d'indicar la mida del password i
+     * el programa ha de generar i mostrar per pantalla el password corresponent. El password pot contenir:
+     * minúscules, majúscules, números i aquests símbols:
+     * ('!','@','#','$','%','^','&','*','(',')','{','}','[',']','=','<','>')
+     *
+     * Hay que subirlo a GitHub y pasar el enlace al mentor por Discord.
+     * Buena suerte!
+     */
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        /**
-         * Es tracta de crear un generador de passwords. L'usuari ha d'indicar la mida del password i
-         * el programa ha de generar i mostrar per pantalla el password corresponent. El password pot contenir:
-         * minúscules, majúscules, números i aquests símbols:
-         * ('!','@','#','$','%','^','&','*','(',')','{','}','[',']','=','<','>')
-         *
-         * Hay que subirlo a GitHub y pasar el enlace al mentor por Discord.
-         * Buena suerte!
-         */
 
         List<String> letras = List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
         List<Integer> numeros = List.of(1,2,3,4,5,6,7,8,9);
         List<Character> characters = List.of('!','@','#','$','%','^','&','*','(',')','{','}','[',']','=','<','>');
+        int length;
 
-        int length = 0;
+        StringBuilder password = new StringBuilder();
         System.out.print("Introduce la longitud: ");
         length = scanner.nextInt();
 
-        StringBuilder password = new StringBuilder();
-
         for(int i=0; i<length; i++){
-            switch (randomNumber()){
+            switch (randomNumberOptions()){
                 case 1:
-                    password.append(letras.get(randomNumberInRange(letras)));
+                    password.append(letras.get(randomNumberRangeListSize(letras)));
                     break;
                 case 2:
-                    password.append(letras.get(randomNumberInRange(letras)).toUpperCase());
+                    password.append(letras.get(randomNumberRangeListSize(letras)).toUpperCase());
                     break;
                 case 3:
-                    password.append(numeros.get(randomNumberInRange(numeros)));
+                    password.append(numeros.get(randomNumberRangeListSize(numeros)));
                     break;
                 case 4:
-                    password.append(characters.get(randomNumberInRange(characters)));
+                    password.append(characters.get(randomNumberRangeListSize(characters)));
                     break;
             }
         }
         System.out.println(password);
 
-
     }
 
-    static int randomNumber(){
+    static int randomNumberOptions(){
         return (int)(Math.random()*4)+1;
     }
 
-    static int randomNumberInRange(List list){
+    static int randomNumberRangeListSize(List list){
         int range = list.size()-1;
         return (int)(Math.random()*range)+1;
     }
